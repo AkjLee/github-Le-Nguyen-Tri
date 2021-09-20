627f4889e7411664d04ec645c3ea074889d0c79c
AkjLee <86882719+AkjLee@users.noreply.github.com>
2021-09-20 14:59:55 +0700

    Update Student.cs

--- Userfrom/Entilien/Student.cs
+++ Userfrom/Entilien/Student.cs
@@ -17,9 +17,43 @@ namespace testapp1.Entilien
         public DateTime DDB { get; internal set; }
         public object Gender { get; internal set; }
 
-        internal Student Get(string idStudent)
+        public static List<Student> GetList()
         {
-            throw new NotImplementedException();
+            var ls = new List<Student>();
+            ls.Add(new Student() {
+                LastName = "Le",
+                Id = "1",
+                FirsName = "Nguyen Tri",
+                DDB = new DateTime(2000,08,02),
+                PDB = "Phu Vang",
+                HomeTown = "Thua Thien Hue",
+                Gender = EGender.Nam,
+
+
+            }) ;
+            ls.Add(new Student() {
+                LastName = "Nguyen",
+                Id = "2",
+                FirsName = "Kim",
+                DDB = new DateTime(2001, 8, 12),
+                PDB = "Phu My",
+                HomeTown = "Thua Thien Hue",
+                Gender = EGender.Nu,
+            });
         }
+
+        public static Student Get(String id)
+        {
+            var dbStudent = GetList();
+            // Ling to SQL
+            var Student = dbStudent.Where(s => s.Id == id).FirstOrDefault();
+            return Student;
+        }
+    }
+
+    public enum EGender
+    {
+        Nam, Nu, Khac
+
     }
 }
