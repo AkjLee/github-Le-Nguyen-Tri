6214005d42d1b9988fbdd75532cb14b2bf29fcad
AkjLee <trilee2912@gmail.com>
2021-09-20 14:56:49 +0700

    Nội dung buổi học thứ 3

---
+++ Userfrom/Entilien/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp1.Entilien
+{
+    class Student
+    {
+        internal string FullName;
+
+        public object LastName { get; internal set; }
+        public object FirsName { get; internal set; }
+        public string PDB { get; internal set; }
+        public string HomeTown { get; internal set; }
+        public DateTime DDB { get; internal set; }
+        public object Gender { get; internal set; }
+
+        internal Student Get(string idStudent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
